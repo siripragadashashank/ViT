98c74c785fba6130d7abdf15073d1ceeeb8051d0
Shashank Siripragada <siripragada.s@northeastern.edu>
2023-04-19 05:59:33 -0400

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -2,7 +2,6 @@ import torch
 import pytorch_lightning as pl
 
 # Torchvision
-import torchvision
 from torchvision.datasets import CIFAR10
 from torchvision import transforms
 
@@ -27,8 +26,6 @@ class ViTDataLoader:
 
         pl.seed_everything(42)
         train_set, _ = torch.utils.data.random_split(train_dataset, [45000, 5000])
-        # pl.seed_everything(42)
-        # _, val_set = torch.utils.data.random_split(val_dataset, [45000, 5000])
 
         train_loader = DataLoader(
             train_set,
@@ -41,12 +38,56 @@ class ViTDataLoader:
 
         return train_loader
 
+    def get_val_loader(self):
 
+        val_transform = transforms.Compose(
+            transforms.ToTensor(),
+        )
+
+        val_dataset = CIFAR10(
+            root=self.dataset_path,
+            train=True,
+            transform=val_transform,
+            download=True
+        )
+
+        pl.seed_everything(42)
+        _, val_set = torch.utils.data.random_split(val_dataset, [45000, 5000])
+        val_loader = DataLoader(
+            val_set,
+            batch_size=4,
+            shuffle=False,
+            drop_last=False,
+            num_workers=4
+        )
+
+        return val_loader
+
+    def get_test_loader(self):
+        test_transform = transforms.Compose([
+            transforms.ToTensor(),
+        ])
+        test_dataset = CIFAR10(
+            root=self.dataset_path,
+            train=False,
+            transform=test_transform,
+            download=True
+        )
+        test_loader = DataLoader(
+            test_dataset,
+            batch_size=4,
+            shuffle=False,
+            drop_last=False,
+            num_workers=4
+        )
+
+        return test_loader
 
 
 if __name__ == '__main__':
     vit_loader = ViTDataLoader(dataset_path='data')
-    train = vit_loader.get_data()
+    train = vit_loader.get_train_loader()
+    val = vit_loader.get_val_loader()
     print(next(iter(train))[0].shape)
 
 
